e5b49c237cdc0922f24edabd6d31710f9dcbc731
sunilpokhrel1 <sunil073bex@ioepc.edu.np>
2022-01-28 02:50:55 +0545

    Added More Features . Made admin roles functional.

--- ElibraryManagement/adminmembermanagement.aspx.cs
+++ ElibraryManagement/adminmembermanagement.aspx.cs
@@ -11,6 +11,37 @@ namespace ElibraryManagement
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            GridView1.DataBind();
+
+        }
+        // add member
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+        //yelllow button
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+        //validate green
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // not validate red
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+        // delete user
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
